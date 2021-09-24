e775462f3a5b26125b6b4b3ae9829fd5622ff5a6
SuperbiaR <altierstuff@gmail.com>
2021-09-24 12:33:38 -0500

    Initial Commit

---
+++ Matching/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matching
+//
+//  Created by PMStudent on 9/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matching/MatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingApp.swift
+//  Matching
+//
+//  Created by PMStudent on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
